ec3bf1e484dc49c017cde9aa4dca04be0cff1490
fierceviking <andersjk123@gmail.com>
2023-11-24 21:50:24 +0100

    split the program into 2 folders
    one folder for a* and another for breadth first

---
+++ breadth_first/breadth_first.py
@@ -0,0 +1,37 @@
+
+
+class breadthFirst:
+    def __init__(self, map, start, end):
+        self.map = map
+        self.start = start
+        self.end = end
+        self.path = []
+        self.queue = []
+        self.visited = []
+        self.prev = {}
+        self.found = False
+    
+    def findPath(self):
+        self.queue.append(self.start)
+        self.visited.append(self.start)
+        while len(self.queue) > 0:
+            current = self.queue.pop(0)
+            if current == self.end:
+                self.found = True
+                break
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    if 0 <= current[0] + dx < len(self.map) and 0 <= current[1] + dy < len(self.map[0]):
+                        if [current[0] + dx, current[1] + dy] not in self.visited and self.map[current[0] + dx][current[1] + dy] != 255:
+                            self.queue.append([current[0] + dx, current[1] + dy])
+                            self.visited.append([current[0] + dx, current[1] + dy])
+                            self.prev[str([current[0] + dx, current[1] + dy])] = current
+        if self.found:
+            self.path.append(self.end)
+            while self.path[-1] != self.start:
+                self.path.append(self.prev[str(self.path[-1])])
+            self.path.reverse()
+            print(self.path)
+            return self.path
+        else:
+            return None
---
+++ breadth_first/map_generator.py
@@ -0,0 +1,32 @@
+import numpy as np
+import random
+
+class MapGenerator:
+    def __init__(self, mapwidth, mapheight, points):
+        self.mapwidth = mapwidth
+        self.mapheight = mapheight
+        self.points = points
+        self.map = np.zeros((mapwidth, mapheight), dtype=np.uint8)
+
+    def generateMap(self):
+        # generate a list of random points
+        for _ in range(self.points):
+            x = random.randint(0, self.mapwidth - 1)
+            y = random.randint(0, self.mapheight - 1)
+            self.map[x][y] = 255
+            # let points adjacent to the random point also have a chance to be set
+            for dx in [-2, -1, 0, 1, 2]:
+                for dy in [-2, -1, 0, 1, 2]:
+                    if 0 <= x + dx < self.mapwidth and 0 <= y + dy < self.mapheight:
+                        if random.random() < 0.5:
+                            self.map[x + dx][y + dy] = 255
+        np.savetxt('breadth_first/map.txt', self.map, fmt='%d')
+        return self.map
+        
+
+if __name__ == '__main__':
+    mapWidth = int(input("Enter map width: "))
+    mapHeight = int(input("Enter map height: "))
+    points = int(input("Enter number of points: "))
+    map = MapGenerator(mapWidth, mapHeight, points).generateMap()
+    print(map)
